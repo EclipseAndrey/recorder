ca2af95eec6bca28d5324036a3a779eefd828ed0
koren <korenkov2002@mail.ru>
2021-03-17 17:34:31 +0300

    time

---
+++ lib/Utils/time/TimeParse.dart
@@ -0,0 +1,84 @@
+
+import 'package:intl/intl.dart';
+
+String txTimeParseDate(String time){
+  DateTime date = DateFormat("yyyy-MM-ddTHH:mm:ss.vvvZ").parse(time);
+  date = date.add(Duration(hours: 3)); // +3 hour to MSK
+  DateTime dateNow= DateTime.now();
+
+  if(date.year == DateTime.now().year && date.month == DateTime.now().month&& date.day == DateTime.now().day){
+    return "Сегодня";
+  }else if(date.year == DateTime.now().year && date.month == DateTime.now().month&& date.day == DateTime.now().day-1){
+    return "Вчера";
+  }
+  return date.day.toString()+"."+date.month.toString();
+}
+
+String txTimeParseTime(String time){
+  DateTime date = DateFormat("yyyy-MM-ddTHH:mm:ss.vvvZ").parse(time);
+  date = date.add(Duration(hours: 3));
+  DateTime dateNow= DateTime.now();
+
+  return setDateMode(date);
+}
+
+String setDateMode(DateTime date) {
+  String ye, mo, da, ho, mi;
+  ye = date.year.toString();
+  mo = date.month < 10 ? "0" + date.month.toString() : date.month.toString();
+  da = date.day < 10 ? "0" + date.day.toString() : date.day.toString();
+  ho = date.hour < 10 ? "0" + date.hour.toString() : date.hour.toString();
+  mi = date.minute % 60 < 10 ? "0" + (date.minute % 60).toString() : (date.minute % 60).toString();
+  return da+ "."+mo+" "+ho + ":" + mi;
+}
+
+
+DateTime dateParse(String time){
+  DateTime date = DateFormat("yyyy-MM-ddTHH:mm:ss.vvvZ").parse(time);
+  date = date.add(Duration(hours: 3));
+  DateTime dateNow= DateTime.now();
+
+  return date;
+}
+
+
+String month(String month) {
+  switch (month) {
+    case "1":
+      return "Янв.";
+      break;
+    case "2":
+      return "Фев.";
+      break;
+    case "3":
+      return "Марта";
+      break;
+    case "4":
+      return "Апр.";
+      break;
+    case "5":
+      return "Мая";
+      break;
+    case "6":
+      return "Июня";
+      break;
+    case "7":
+      return "Июля";
+      break;
+    case "8":
+      return "Авг.";
+      break;
+    case "9":
+      return "Сен.";
+      break;
+    case "10":
+      return "Окт.";
+      break;
+    case "11":
+      return "Ноя.";
+      break;
+    case "12":
+      return "Дек.";
+      break;
+  }
+}
